b366aed11d324d629b327e0be16b1f8437ab1446
Naitik Yadav <ntkydv16@gmail.com>
2025-05-09 22:38:59 +0530

    update

--- Products/models.py
+++ Products/models.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 from utils.base_models import CreatedUpdatedOnMixin, RequestResponseMixin
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-from cloudinary.models import CloudinaryField
 
 
 def validate_file_extension(value):
@@ -75,7 +74,7 @@ class Products(CreatedUpdatedOnMixin, RequestResponseMixin):
     description = models.CharField(max_length=225)
     category = models.CharField(max_length=225)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = CloudinaryField('image', blank=True, null=True)
+    image = models.FileField(upload_to='products/', blank=True, null=True,validators=[validate_file_extension])
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
 
